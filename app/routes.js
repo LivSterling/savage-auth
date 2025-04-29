405272eb54f05fbbb6867fd8241fc9079608e295
Sterling <sterlingchapmanmade@gmail.com>
2025-04-29 12:07:03 -0400

    lil

--- app/routes.js
+++ app/routes.js
@@ -43,7 +43,7 @@ module.exports = function(app, passport, db) {
       .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
         $set: {
           thumbUp:req.body.thumbUp + 1
-        }
+        } 
       }, {
         sort: {_id: -1},
         upsert: true
